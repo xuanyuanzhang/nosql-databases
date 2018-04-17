ebf5ef8e2a367feb90bf992927947be6d1d0e8c9
xuanyuanzhang <xz2580@columbia.edu>
2018-04-17 17:35:46 -0400

    finish

--- mongodb/homework_3.py
+++ mongodb/homework_3.py
@@ -30,12 +30,41 @@ drama_movies = db.movie_collection.aggregate([
 	{"$match": {"genres": "Drama"}},
 	{"$group": {"_id": "Drama", "count": {"$sum": 1}}}
 ])
+print("movies in drama")
+for ele in drama_movies:
+	pprint(ele)
 
 # 4. find number of movies made in China
+# this is kinda tricky.
+# What I get here are the movies only produced by China.
+# without "[]", we will get all movies including China
+# as one of the countries.
+# It is not specified clearly what is expected.
 china_movies = db.movie_collection.aggregate([
         {"$match": {"countries": ["China"], "rated": "Pending rating"}},
         {"$group": {"_id": {"countries": ["China"], "rated": "Pending rating"}, "count": {"$sum": 1}}}
 ])
+print("movies in china")
+for ele in china_movies:
+	pprint(ele)
 
 # 5. test lookup and return at least two results
+db.team.insert([
+	{"group": "RNG", "members": ["Xiaohu", "MLXG", "UZI", "Ming", "ZziTai"]},
+	{"group": "WE", "members": ["Xiye", "Mystic", "Condi", "957", "Magic"]},
+	{"group": "EDG", "members": ["Clearlove7", "Meiko", "Mouse", "Scout", "Audi"]}
+])
 
+db.rank2.insert([
+	{"group": "WE", "rank": 7},
+	{"group": "IG", "rank": 1},
+	{"group": "EDG", "rank": 2},
+	{"group": "RNG", "rank": 3}
+])
+
+league_cursor = db.team.aggregate([
+	{"$lookup": {"from": "rank2", "localField": "group", "foreignField": "group", "as": "rank"}}
+])
+
+for ele in league_cursor:
+	pprint(ele)
